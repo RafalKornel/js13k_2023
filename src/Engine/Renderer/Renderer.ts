b7b7e1ddb064cfd6300129b0e5de988fc9ebb28d
RafalKornel <rafal.kornel123@gmail.com>
2023-08-29 17:22:14 +0200

    refactor: decouple text renderer from base renderer

--- src/Engine/Renderer/Renderer.ts
+++ src/Engine/Renderer/Renderer.ts
@@ -1,19 +1,11 @@
-import { TEXT_CONFIG } from "../config";
-import { Anchor, Direction, Interaction, TextSize, Vec2 } from "../types";
+import { Anchor, Direction, Interaction, Vec2 } from "../types";
 import { Assets, Colors } from "./types";
 
-import { add, convertTileVecToGlobal, mult, subtract } from "../utils";
-import { CONFIG } from "../config";
 import {
   CanvasImageRenderer,
   ICanvasImageRenderer,
 } from "./CanvasImageRenderer";
-
-type DrawTextParams = {
-  color?: string;
-  backgroundColor?: string;
-  anchor?: "left" | "center";
-};
+import { DrawTextParams, ITextRenderer, TextRenderer } from "./TextRenderer";
 
 export type RenderEngineParams = {
   width?: number;
@@ -24,12 +16,13 @@ export abstract class Renderer {
   private _raf?: number;
 
   protected ctx: CanvasRenderingContext2D;
-  protected textCtx: CanvasRenderingContext2D;
 
   protected width: number;
   protected height: number;
 
   private _canvasImageRenderer: ICanvasImageRenderer;
+
+  private _textRenderer: ITextRenderer;
 
   /** Main entry point to renderer - implement this in children classes, by
    * modifying buffer property.
@@ -37,8 +30,8 @@ export abstract class Renderer {
   abstract loop(): void;
 
   constructor(
-    readonly gameCanvas: HTMLCanvasElement,
-    readonly textCanvas: HTMLCanvasElement,
+    gameCanvas: HTMLCanvasElement,
+    textCanvas: HTMLCanvasElement,
     colors: Colors,
     assets: Assets,
     options: RenderEngineParams = {}
@@ -51,13 +44,9 @@ export abstract class Renderer {
     gameCanvas.width = this.width;
     gameCanvas.height = this.height;
 
-    textCanvas.width = gameCanvas.clientWidth;
-    textCanvas.height = gameCanvas.clientHeight;
-
     const gameCtx = gameCanvas.getContext("2d");
-    const textCtx = textCanvas.getContext("2d");
 
-    if (!gameCtx || !textCtx) {
+    if (!gameCtx) {
       throw new Error("Your browser doesn't support canvas");
     }
 
@@ -65,13 +54,34 @@ export abstract class Renderer {
 
     this.ctx = gameCtx;
 
-    this.textCtx = textCtx;
+    const scale = gameCanvas.clientWidth / this.width;
+
+    this._textRenderer = new TextRenderer(
+      textCanvas,
+      gameCanvas.clientWidth,
+      gameCanvas.clientHeight,
+      scale
+    );
 
     this._canvasImageRenderer = new CanvasImageRenderer(
       this.ctx,
       colors,
       assets
     );
+  }
+
+  drawText(
+    text: string,
+    size: "m" | "l",
+    gameX: number,
+    gameY: number,
+    options?: DrawTextParams
+  ) {
+    this._textRenderer.drawText(text, size, gameX, gameY, options);
+  }
+
+  dialogueModal(left: string[], options: Interaction[]) {
+    this._textRenderer.dialogueModal(left, options);
   }
 
   renderImage(
@@ -109,112 +119,6 @@ export abstract class Renderer {
     this.ctx.fillRect(x - xOffset, y - yOffset, w, h);
   };
 
-  get scale() {
-    return this.textCanvas.width / this.gameCanvas.width;
-  }
-
-  drawText(
-    text: string,
-    size: "m" | "l",
-    gameX: number,
-    gameY: number,
-    options?: DrawTextParams
-  ) {
-    const {
-      color = TEXT_CONFIG.color,
-      anchor = "center",
-      backgroundColor,
-    } = options || {};
-
-    const [lineHeight] = this.setupTextProperties(size, color);
-
-    const textWidth = this.textCtx.measureText(text).width;
-
-    const xOff = anchor === "center" ? textWidth / 2 : 0;
-
-    const textX = gameX * this.scale - xOff;
-    const textY = gameY * this.scale;
-
-    if (backgroundColor) {
-      this.textCtx.fillStyle = backgroundColor;
-      this.textCtx.fillRect(
-        textX - TEXT_CONFIG.margin,
-        textY - TEXT_CONFIG.margin,
-        textWidth + TEXT_CONFIG.margin * 2,
-        lineHeight + TEXT_CONFIG.margin
-      );
-      this.textCtx.fillStyle = color;
-    }
-
-    this.textCtx.fillText(text, textX, textY);
-  }
-
-  dialogueModal(left: string[], options: Interaction[]) {
-    const size = mult(convertTileVecToGlobal([8, 6]), this.scale);
-
-    const pos = mult(
-      convertTileVecToGlobal([CONFIG.width / 2, CONFIG.height / 2]),
-      this.scale
-    );
-
-    const halfOffset: Vec2 = [size[0] / 2, 0];
-
-    const textBox = subtract(pos, mult(size, 0.5));
-
-    const textBoxWithMargin = add(textBox, [
-      TEXT_CONFIG.margin,
-      TEXT_CONFIG.margin,
-    ]);
-
-    const ctx = this.textCtx;
-
-    ctx.strokeStyle = TEXT_CONFIG.borderColor;
-    ctx.fillStyle = TEXT_CONFIG.borderBackground;
-
-    ctx.lineWidth = 2;
-
-    ctx.strokeRect(...textBox, ...size);
-    ctx.fillRect(...textBox, ...size);
-
-    ctx.strokeRect(...add(textBox, halfOffset), 0, size[1]);
-
-    const [lineHeight] = this.setupTextProperties("m", TEXT_CONFIG.color);
-
-    const lines = left.map((value) => value.split("\n")).flat();
-
-    lines.forEach((line, i) => {
-      ctx.fillText(line, ...add(textBoxWithMargin, [0, lineHeight * i]));
-    });
-
-    options.forEach((line, i) => {
-      ctx.fillText(
-        `${line.key}: ${line.text}`,
-        ...add(textBoxWithMargin, [size[0] / 2, lineHeight * i])
-      );
-    });
-
-    ctx.fillText(
-      "x: close",
-      ...add(textBoxWithMargin, [size[0] / 2, size[1] - lineHeight * 2])
-    );
-  }
-
-  private setupTextProperties(size: TextSize, color: string) {
-    const fontSize = TEXT_CONFIG.fontSize[size];
-
-    this.textCtx.font = `${fontSize}px ${TEXT_CONFIG.fontFace}`;
-
-    this.textCtx.textAlign = TEXT_CONFIG.textAlign;
-
-    this.textCtx.textBaseline = TEXT_CONFIG.textBaseline;
-
-    this.textCtx.fillStyle = color;
-
-    const lineHeight = TEXT_CONFIG.lineHeight * fontSize;
-
-    return [lineHeight];
-  }
-
   public start() {
     this._raf = this.loopWrapper();
   }
@@ -237,6 +141,7 @@ export abstract class Renderer {
 
   private clear() {
     this.ctx.clearRect(0, 0, this.width, this.height);
-    this.textCtx.clearRect(0, 0, this.textCanvas.width, this.textCanvas.height);
+
+    this._textRenderer.clear();
   }
 }
---
+++ src/Engine/Renderer/TextRenderer.ts
@@ -0,0 +1,146 @@
+import { TEXT_CONFIG } from "../config";
+import { Interaction, TextSize, Vec2 } from "../types";
+import { add, convertTileVecToGlobal, mult, subtract } from "../utils";
+import { CONFIG } from "../config";
+
+export type DrawTextParams = {
+  color?: string;
+  backgroundColor?: string;
+  anchor?: "left" | "center";
+};
+
+export interface ITextRenderer {
+  drawText(
+    text: string,
+    size: "m" | "l",
+    gameX: number,
+    gameY: number,
+    options?: DrawTextParams
+  ): void;
+
+  dialogueModal(left: string[], options: Interaction[]): void;
+
+  clear(): void;
+}
+
+export class TextRenderer implements ITextRenderer {
+  protected textCtx: CanvasRenderingContext2D;
+
+  constructor(
+    readonly textCanvas: HTMLCanvasElement,
+    readonly width: number,
+    readonly height: number,
+    readonly scale: number
+  ) {
+    textCanvas.width = width;
+    textCanvas.height = height;
+
+    this.textCtx = textCanvas.getContext("2d")!;
+  }
+
+  drawText(
+    text: string,
+    size: "m" | "l",
+    gameX: number,
+    gameY: number,
+    options?: DrawTextParams
+  ) {
+    const {
+      color = TEXT_CONFIG.color,
+      anchor = "center",
+      backgroundColor,
+    } = options || {};
+
+    const [lineHeight] = this.setupTextProperties(size, color);
+
+    const textWidth = this.textCtx.measureText(text).width;
+
+    const xOff = anchor === "center" ? textWidth / 2 : 0;
+
+    const textX = gameX * this.scale - xOff;
+    const textY = gameY * this.scale;
+
+    if (backgroundColor) {
+      this.textCtx.fillStyle = backgroundColor;
+      this.textCtx.fillRect(
+        textX - TEXT_CONFIG.margin,
+        textY - TEXT_CONFIG.margin,
+        textWidth + TEXT_CONFIG.margin * 2,
+        lineHeight + TEXT_CONFIG.margin
+      );
+      this.textCtx.fillStyle = color;
+    }
+
+    this.textCtx.fillText(text, textX, textY);
+  }
+
+  dialogueModal(left: string[], options: Interaction[]) {
+    const size = mult(convertTileVecToGlobal([8, 6]), this.scale);
+
+    const pos = mult(
+      convertTileVecToGlobal([CONFIG.width / 2, CONFIG.height / 2]),
+      this.scale
+    );
+
+    const halfOffset: Vec2 = [size[0] / 2, 0];
+
+    const textBox = subtract(pos, mult(size, 0.5));
+
+    const textBoxWithMargin = add(textBox, [
+      TEXT_CONFIG.margin,
+      TEXT_CONFIG.margin,
+    ]);
+
+    const ctx = this.textCtx;
+
+    ctx.strokeStyle = TEXT_CONFIG.borderColor;
+    ctx.fillStyle = TEXT_CONFIG.borderBackground;
+
+    ctx.lineWidth = 2;
+
+    ctx.strokeRect(...textBox, ...size);
+    ctx.fillRect(...textBox, ...size);
+
+    ctx.strokeRect(...add(textBox, halfOffset), 0, size[1]);
+
+    const [lineHeight] = this.setupTextProperties("m", TEXT_CONFIG.color);
+
+    const lines = left.map((value) => value.split("\n")).flat();
+
+    lines.forEach((line, i) => {
+      ctx.fillText(line, ...add(textBoxWithMargin, [0, lineHeight * i]));
+    });
+
+    options.forEach((line, i) => {
+      ctx.fillText(
+        `${line.key}: ${line.text}`,
+        ...add(textBoxWithMargin, [size[0] / 2, lineHeight * i])
+      );
+    });
+
+    ctx.fillText(
+      "x: close",
+      ...add(textBoxWithMargin, [size[0] / 2, size[1] - lineHeight * 2])
+    );
+  }
+
+  clear() {
+    this.textCtx.clearRect(0, 0, this.textCanvas.width, this.textCanvas.height);
+  }
+
+  private setupTextProperties(size: TextSize, color: string) {
+    const fontSize = TEXT_CONFIG.fontSize[size];
+
+    this.textCtx.font = `${fontSize}px ${TEXT_CONFIG.fontFace}`;
+
+    this.textCtx.textAlign = TEXT_CONFIG.textAlign;
+
+    this.textCtx.textBaseline = TEXT_CONFIG.textBaseline;
+
+    this.textCtx.fillStyle = color;
+
+    const lineHeight = TEXT_CONFIG.lineHeight * fontSize;
+
+    return [lineHeight];
+  }
+}
