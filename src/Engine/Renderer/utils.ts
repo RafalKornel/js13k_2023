e10ff178bd183f116e59735fef07503c89342b05
RafalKornel <rafal.kornel123@gmail.com>
2025-08-21 00:24:43 +0200

    rm

--- src/Engine/Renderer/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Direction } from "../types";
-import { ImageCacheKey } from "./types";
-
-export function getImageCacheKey(
-  imageId: number,
-  dir: Direction
-): ImageCacheKey {
-  return `${imageId} | ${dir}`;
-}
--- src/Game/Scenes/emptyScene.ts
+++
@@ -1,6 +0,0 @@
-import { Scene } from "../../Engine/Scene/Scene";
-import { createScenePositionComponent } from "../helpers";
-import { SCENE_KEYS } from "./constants";
-
-export const createEmptyScene = () =>
-  new Scene(SCENE_KEYS.empty, createScenePositionComponent());
